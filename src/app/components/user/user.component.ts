4db5a49aaea0cec36ff1755118729c771fb38228
M Haseeb Ur Rehman <mhaseeburrehman123>
2025-02-26 13:04:39 +0500

    Previous Sharing

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -19,12 +19,21 @@ export class UserComponent {
     { name: 'Laptop', price: 999, image: 'assets/LaptopImage.jpeg', quantity: 1 },
     { name: 'Smartphone', price: 599, image: 'assets/PhoneImage.jpg', quantity: 1 },
     { name: 'Headphones', price: 199, image: 'assets/HeadphoneImage.jpeg', quantity: 1 },
-    {name:'WirelessMouse',price:30,image:'assets/mouse-image.jpeg',quantity:1}
-  ];
+    {name:'WirelessMouse',price:30,image:'assets/mouse-image.jpeg',quantity:1},
+    {name:'Double Bed',price:500,image:'assets/Double Bed.jpeg',quantity:1},
+    {name:'Sofa Chair',price:200,image:'assets/Sofa Chair.jpeg',quantity:1},
+    {name:'Table',price:100,image:'assets/Table.jpeg',quantity:1},
+    {name:'Meeting Table',price:600,image:'assets/Meeting Table.jpeg',quantity:1},
+    {name:'Galaxy Bags',price:200,image:'assets/glaxybags.jpeg',quantity:1},
+    {name:'Shoulder Bags',price:250,image:'assets/shoulderbags.jpeg',quantity:1},
+    {name:'Stylish Lightweight Bags',price:300,image:'assets/stylishlightweight.jpeg',quantity:1},
+    {name:'Ladies Bags',price:300,image:'assets/ladiesbags.jpeg',quantity:1},
+    {name:'Relax T Shirt',price:100,image:'assets/relaxfittshirt.jpeg',quantity:1},
+    {name:'Plain Red Shirt',price:120,image:'assets/Plainredshirt.jpeg',quantity:1},
+    {name:'Plain Black Shirt',price:130,image:'assets/plainblacktshirt.jpeg',quantity:1},
+    {name:'Alone T Shirt',price:170,image:'assets/AloneTshirt.jpeg',quantity:1}
 
-  // increaseQuantity(product: any) {
-  //  this.cartService.increaseQuantity(product);
-  // }
+  ];
   increaseQuantity(product: any){
     
       this.cartService.increaseQuantity(product);
@@ -39,7 +48,7 @@ export class UserComponent {
 
   addToCart(product: any) {
     this.cartService.addToCart(product);
-    this.router.navigate([`/cart`]);    
+    this.router.navigate([`/cart`]);     
     //alert(`${product.name} added to cart!`);
  }
 }
