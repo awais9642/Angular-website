fe84127c51f336010c048ef57a1b4f067dfb395d
awais9642 <awaisshakir679@gmail.com>
2025-03-06 12:30:35 +0500

    cartcomponent changes

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -14,24 +14,27 @@ import { MatButtonModule } from '@angular/material/button';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+
+ 
+
   cartDialogRef: MatDialogRef<CartComponent> | null = null;
   products = [
     { name: 'Laptop', price: 999, image: 'assets/LaptopImage.jpeg', quantity: 1 },
     { name: 'Smartphone', price: 599, image: 'assets/PhoneImage.jpg', quantity: 1 },
-    { name: 'Headphones', price: 199, image: 'assets/HeadphoneImage.jpeg', quantity: 1 },
-    {name:'WirelessMouse',price:30,image:'assets/mouse-image.jpeg',quantity:1},
+    { name: 'Headphone', price: 199, image: 'assets/HeadphoneImage.jpeg', quantity: 1 },
+    {name:'Wireless Mouse',price:30,image:'assets/mouse-image.jpeg',quantity:1},
     {name:'Double Bed',price:500,image:'assets/Double Bed.jpeg',quantity:1},
     {name:'Sofa Chair',price:200,image:'assets/Sofa Chair.jpeg',quantity:1},
     {name:'Table',price:100,image:'assets/Table.jpeg',quantity:1},
     {name:'Meeting Table',price:600,image:'assets/Meeting Table.jpeg',quantity:1},
-    {name:'Galaxy Bags',price:200,image:'assets/glaxybags.jpeg',quantity:1},
-    {name:'Shoulder Bags',price:250,image:'assets/shoulderbags.jpeg',quantity:1},
-    {name:'Stylish Lightweight Bags',price:300,image:'assets/stylishlightweight.jpeg',quantity:1},
-    {name:'Ladies Bags',price:300,image:'assets/ladiesbags.jpeg',quantity:1},
-    {name:'Relax T Shirt',price:100,image:'assets/relaxfittshirt.jpeg',quantity:1},
+    {name:'Galaxy Bag',price:200,image:'assets/glaxybags.jpeg',quantity:1},
+    {name:'Shoulder Bag',price:250,image:'assets/shoulderbags.jpeg',quantity:1},
+    {name:'Stylish Bag',price:300,image:'assets/stylishlightweight.jpeg',quantity:1},
+    {name:'Ladies Bag',price:300,image:'assets/ladiesbags.jpeg',quantity:1},
+    {name:'Relax T-Shirt',price:100,image:'assets/relaxfittshirt.jpeg',quantity:1},
     {name:'Plain Red Shirt',price:120,image:'assets/Plainredshirt.jpeg',quantity:1},
     {name:'Plain Black Shirt',price:130,image:'assets/plainblacktshirt.jpeg',quantity:1},
-    {name:'Alone T Shirt',price:170,image:'assets/AloneTshirt.jpeg',quantity:1}
+    {name:'Alone T-Shirt',price:170,image:'assets/AloneTshirt.jpeg',quantity:1}
 
   ];
   constructor(private dialog: MatDialog,private cartService: CartServiceService,private router: Router){}
@@ -44,6 +47,8 @@ export class UserComponent {
         panelClass: 'custom-dialog-container',
         data: { cartItems: this.products }
       });
+
+    
 
       // Reset reference when the dialog is closed
       this.cartDialogRef.afterClosed().subscribe(() => {
