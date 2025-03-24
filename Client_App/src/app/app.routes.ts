ad01912a95e47205efb264d8d622a5edfbb1bf2b
M Haseeb Ur Rehman <mhaseeburrehman123>
2025-03-24 12:04:18 +0500

    nc

--- Client_App/src/app/app.routes.ts
+++ Client_App/src/app/app.routes.ts
@@ -5,7 +5,6 @@ import { UserComponent } from './components/user/user.component';
 import { CartComponent } from './components/cart/cart.component';
 import { CartformComponent } from './components/cartform/cartform.component';
 import { ConfirmOrderComponent } from './confirm-order/confirm-order.component';
-import { AuthGuard } from './auth/auth.guard'; // Import AuthGuard
 import { AdminComponent } from './admin/admin.component';
 
 //The canActivate: [AuthGuard] ensures that only logged-in users can access certain routes.
@@ -13,11 +12,11 @@ import { AdminComponent } from './admin/admin.component';
 export const routes: Routes = [
   { path: '', redirectTo: 'auth', pathMatch: 'full' }, 
   { path: 'auth', component: LoginComponent }, // Login page (public)
-  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] }, // Protected
-  { path: 'user', component: UserComponent, canActivate: [AuthGuard] },
-  { path: 'cart', component: CartComponent, canActivate: [AuthGuard] },
-  { path: 'cartform', component: CartformComponent, canActivate: [AuthGuard] },
-  { path: 'confirmorder', component: ConfirmOrderComponent, canActivate: [AuthGuard] },
+  { path: 'dashboard', component: DashboardComponent,  }, // Protected
+  { path: 'user', component: UserComponent, },
+  { path: 'cart', component: CartComponent,  },
+  { path: 'cartform', component: CartformComponent, },
+  { path: 'confirmorder', component: ConfirmOrderComponent,  },
   { path: 'admin', component: AdminComponent },  // New Admin Route
 ];
 
